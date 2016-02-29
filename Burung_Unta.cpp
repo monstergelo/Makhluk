294271bf64b2e2057dfd9acb57fcb19c20954d0a
harryawk <harry.kambey@gmail.com>
2016-02-29 23:48:42 +0700

    Implementasi Service function Burung_Unta

--- Burung_Unta.cpp
+++ Burung_Unta.cpp
@@ -1,3 +1,19 @@
 #include "Burung_Unta.h"
 
-bool Burung_Unta::berlari()
+bool Burung_Unta::berlari() {
+  //untuk cek petak-petak di sekitar hewan
+  bool isExist = false;
+  for (int delx = -1; delx < 2; delx++) {
+    for (int del y = -1; dely < 2; dely++) {
+      if (delx != 0 && dely != 0) { //tidak melakukan pemeriksaan terhadap petak tempat objek ini berada
+        if (/*Getter posisi + x dan + y ada objek yang merupakan member dari char* predator*/) {
+          isExist = true;
+          break;
+        } else {
+          //checking
+        }
+      }
+    }
+  }
+  return isExist;
+}
