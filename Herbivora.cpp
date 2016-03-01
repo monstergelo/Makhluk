1c6e4a7aa7a2e2266a5888ac6b1d2f7e79dd7320
harryawk <harry.kambey@gmail.com>
2016-03-01 17:40:57 +0700

    Update Herbivora.cpp

--- Herbivora.cpp
+++ Herbivora.cpp
@@ -1,24 +1,18 @@
 #include "Herbivora.h"
 
-bool Gajah::berlari() {
-    bool out_loop = false;
-    bool bahaya = false;
-    size_t x = strlen(predator);
-    unsigned banyak_predator = x;
-    int i=0;
-    while (!out_loop && i <= banyak_predator) {
-        point predator_pos = posisi.predator[i];
-        if (/*Kondisi predator ada di sekitar Gajah*/) {
-            bahaya = true;
-            out_loop = true;
-        } else {
-            //do nothing
-        }
-        i++;
-    }
-    return bahaya;
+Herbivora::Herbivora() {
+    //inisialisasi data 
 }
 
-bool Burung_Unta::berlari() {
-    //sama dengan Gajah::berlari() (?)
+Herbivora::~Herbivora() {
+    
 }
+
+Herbivora::Herbivora(const Herbivora& H) {
+    
+}
+
+Herbivora& Herbivora::operator=(const Herbivora& H) {
+    
+}
+
