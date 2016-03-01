20236a1a6e4acd64e65c88b3556dbdca6066dc0e
harryawk <harry.kambey@gmail.com>
2016-03-01 18:13:13 +0700

    Update Burung_Unta.h

--- Burung_Unta.h
+++ Burung_Unta.h
@@ -7,5 +7,16 @@ public :
   Burung_Unta(const Burung_Unta&);
   Burung_Unta& operator= (const Burung_Unta&);
   
-  bool berlari();
+    //services
+  void menua();
+  //menambah umur dari makhluk hidup
+  void gerak();
+  //mengbah posisi makhluk hidup sesuai arah dan kecepatan
+  bool mati();
+  //mengembalikan nilai true jika umur makhluk hidup sudah mencapai batas umur atau kondisi tertentu;
+  void display();
+  //menampilkan makhluk hidup ke layar
+  bool Lapar();
+  bool memburu();
+
 }
