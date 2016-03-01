051d1a18c4cb4f498c3a66346de517caaa788347
harryawk <harry.kambey@gmail.com>
2016-03-01 17:50:19 +0700

    Update Gajah.cpp

--- Gajah.cpp
+++ Gajah.cpp
@@ -1,3 +1,17 @@
 #include "Gajah.h"
 
-Gajah::Gajah() {}
+Gajah::Gajah() {
+  
+}
+
+Gajah::~Gajah() {
+  
+}
+
+Gajah::Gajah(const Gajah& G) {
+  
+}
+
+Gajah& Gajah::operator= (const Gajah& G) {
+  
+}
