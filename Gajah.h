20b9546cc26d8c9758523a7094f222600e99c747
harryawk <harry.kambey@gmail.com>
2016-03-01 18:09:30 +0700

    Update Gajah.h

--- Gajah.h
+++ Gajah.h
@@ -8,5 +8,15 @@ public :
   Gajah(const Gajah&);
   Gajah& operator= (const Gajah&);
   
+  //services
+  void menua() 
+  //menambah umur dari makhluk hidup
+  void gerak()
+  //mengbah posisi makhluk hidup sesuai arah dan kecepatan
+  bool mati()
+  //mengembalikan nilai true jika umur makhluk hidup sudah mencapai batas umur atau kondisi tertentu;
+  void display()
+  //menampilkan makhluk hidup ke layar
+  
   void berlari();
 }
