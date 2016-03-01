91a03cb512139e5ac275664a4b3b73193a790cd3
harryawk <harry.kambey@gmail.com>
2016-03-01 19:28:13 +0700

    Update Herbivora.cpp

--- Herbivora.cpp
+++ Herbivora.cpp
@@ -16,3 +16,34 @@ Herbivora& Herbivora::operator=(const Herbivora& H) {
     
 }
 
+ifstream Herbivora::operator>> (istream&){
+    
+}
+ofstream Herbivora::operator<< (ostream&){
+    
+}
+
+//services
+void Herbivora::menua(){
+    
+}
+//menambah umur dari makhluk hidup
+void Herbivora::gerak(){
+    
+}
+//mengbah posisi makhluk hidup sesuai arah dan kecepatan
+bool Herbivora::mati(){
+    
+}
+//mengembalikan nilai true jika umur makhluk hidup sudah mencapai batas umur atau kondisi tertentu;
+void Herbivora::display(){
+    
+}
+//menampilkan makhluk hidup ke layar
+bool Herbivora::Lapar(){
+    
+}
+bool Herbivora::memburu(){
+    
+}
+
