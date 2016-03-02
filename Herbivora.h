462f961160f0ae5b1052f1df649b7f7090bc31a6
unknown <harry.kambey@gmail.com>
2016-03-03 01:50:00 +0700

    Fungsi selain 4 sekawan : bergerak, isTarget, isPredator, berlari. Tambah getter setter delta_kecepatan

--- Herbivora.h
+++ Herbivora.h
@@ -3,16 +3,35 @@
 
 #include "Hewan.h"
 
+//Catatan : Hewan inherit ke MakhlukHidup, Gerak, dan HuntingSkill
 class Herbivora : public Hewan {
 public :
-    //5 sekawan
-    Herbivora();
-    ~Herbivora();
+    //4 sekawan
+    Herbivora(int kec = 0);
+    virtual ~Herbivora();
     Herbivora(const Herbivora&);
     Herbivora& operator=(const Herbivora&);
-    ifstream operator>> (istream&);
-    ofstream operator<< (ostream&);
+    
+    //getter-setter
+    virtual int get_deltaKecepatan();
+    virtual void Herbivora::set_deltaKecepatan(int _deltaKecepatan);
 
+    //services
+    void bergerak(MakhlukHidup M);
+    /*
+    Mengubah posisi dari Herbivora ke posisi M jika Herbivora merupakan predator dari M.
+    Mengubah posisi secara acak apabila Herbivora bukan predator dari M.
+    */
+    bool isTarget(MakhlukHidup M);
+    /*
+    Menghasilkan nilai true jika MakhlukHidup M mempunyai predator Herbivora. Hal ini
+    akan membantu Herbivora dalam mencari makanan (berburu).
+    */
+    bool isPredator(MakhlukHidup M);
+    /*
+    Menghasilkan nilai true jika MakhlukHidup M adalah predator dari Herbivora. Hal
+    ini akan membantu penyadaran Herbivora terhadap MakhlukHidup yang berada di sekitarnya.
+    */
     bool berlari();
     /*
     Menghasilkan nilai true jika herbivora menyadari predatornya ada di sekitarnya.
