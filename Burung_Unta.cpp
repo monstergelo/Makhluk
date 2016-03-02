d8310d3556ef2f456a14d443a11d99108b90ffaa
harryawk <harry.kambey@gmail.com>
2016-03-02 12:43:02 +0700

    Update Burung_Unta.cpp

--- Burung_Unta.cpp
+++ Burung_Unta.cpp
@@ -1,5 +1,39 @@
 #include "Burung_Unta.h"
 
+Burung_Unta::Burung_Unta(){
+  
+}
+Burung_Unta::~Burung_Unta(){
+  
+}
+Burung_Unta::Burung_Unta(const Burung_Unta&){
+  
+}
+Burung_Unta& Burung_Unta::operator= (const Burung_Unta&){
+  
+}
+ifstream Burung_Unta::operator>> (istream&){
+  
+}
+ofstream Burung_Unta::operator<< (ostream&){
+  
+}
+
+//services
+//menambah umur dari makhluk hidup
+void Burung_Unta::gerak(){
+  
+}
+//mengbah posisi makhluk hidup sesuai arah dan kecepatan
+//mengembalikan nilai true jika umur makhluk hidup sudah mencapai batas umur atau kondisi tertentu;
+//menampilkan makhluk hidup ke layar
+bool Burung_Unta::Lapar(){
+  
+}
+bool Burung_Unta::memburu(){
+  
+}
+
 bool Burung_Unta::berlari() {
   //untuk cek petak-petak di sekitar hewan
   bool isExist = false;
