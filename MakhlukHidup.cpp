eb9eb607e9cf95decb4c2430b068d60a0b61af6e
aliakbr <aliraal12@gmail.com>
2016-03-03 08:32:06 +0700

    Perubahan sedikit dari versi sebelumnya

--- MakhlukHidup.cpp
+++ MakhlukHidup.cpp
@@ -1,12 +1,15 @@
 #include "MakhlukHidup.h"
 
 MakhlukHidup::MakhlukHidup(int _umur, char _DNA, int _ulangtahun, Point P) : ukuran_predator(20), batas_umur(_umur), posisi(P.getAbsis(),P.getOrdinat()), PPosisi(P.getAbsis(),P.getOrdinat()) {
+    umur = 0;
     DNA = _DNA;
     ulang_tahun = _ulangtahun;
     predator = new char[ukuran_predator];
 }
 
-MakhlukHidup::MakhlukHidup(const MakhlukHidup& M) : ukuran_predator(20), batas_umur(M.batas_umur){
+MakhlukHidup::MakhlukHidup(const MakhlukHidup& M) : ukuran_predator(M.ukuran_predator), batas_umur(M.batas_umur){
+    umur = M.umur;
+    ulang_tahun = M.ulang_tahun;
     DNA = M.DNA;
     posisi  = M.posisi;
     PPosisi = M.PPosisi;
@@ -35,47 +38,47 @@ MakhlukHidup& MakhlukHidup::operator=(const MakhlukHidup& M){
 }
 
 
-    //getter
-    int MakhlukHidup::get_umur()
-    {
-    	return umur;
-    }
+//getter
+int MakhlukHidup::get_umur()
+{
+    return umur;
+}
 
-    int MakhlukHidup::get_ulang_tahun()
-    {
-    	return ulang_tahun;
-    }
+int MakhlukHidup::get_ulang_tahun()
+{
+    return ulang_tahun;
+}
 
-    char MakhlukHidup::get_DNA()
-    {
-    	return DNA;
-    }
+char MakhlukHidup::get_DNA()
+{
+    return DNA;
+}
 
-    int MakhlukHidup::get_batas_umur()
-    {
-    	return batas_umur;
-    }
+int MakhlukHidup::get_batas_umur()
+{
+    return batas_umur;
+}
 
-    Point MakhlukHidup::getPosisi(){
-        return posisi;
-    }
+Point MakhlukHidup::getPosisi(){
+    return posisi;
+}
 
-    //setter
-    void MakhlukHidup::set_umur(int i)
-    {
-    	umur = i;
-    }
+//setter
+void MakhlukHidup::set_umur(int i)
+{
+    umur = i;
+}
 
-    void MakhlukHidup::set_ulang_tahun(int i)
-    {
-    	ulang_tahun = i;
-    }
+void MakhlukHidup::set_ulang_tahun(int i)
+{
+    ulang_tahun = i;
+}
 
-    void MakhlukHidup::set_DNA(char i)
-    {
-    	DNA = i;
-    }
+void MakhlukHidup::set_DNA(char i)
+{
+    DNA = i;
+}
 
-    void MakhlukHidup::setPosisi(Point P){
-        posisi = P;
-    }
+void MakhlukHidup::setPosisi(Point P){
+    posisi = P;
+}
--- driverMakhlukHidup.cpp
+++ driverMakhlukHidup.cpp
@@ -2,7 +2,16 @@
 #include <iostream>
 using namespace std;
 main(){
+    Point P1(1,0);
+    MakhlukHidup M1(10,'a',9,P1);
     MakhlukHidup M;
     char k = M.get_DNA();
     cout << k << endl;
+    cout << M.get_umur() << endl;
+    cout << M.get_batas_umur() << endl;
+    M = M1;
+    k = M.get_DNA();
+    cout << k << endl;
+    cout << M1.get_umur() << endl;
+    cout << M1.get_batas_umur() << endl;
 }
