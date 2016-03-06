e0936bfc6ed2884fbd77ebe756cf0c49c87c688d
Aditio Pangestu <aditiopangestu@gmail.com>
2016-03-06 12:52:03 +0700

    Update masih belum final driver manusia

--- driverManusia.cpp
+++ driverManusia.cpp
@@ -5,6 +5,36 @@ using namespace std;
 
 int main () {
     Manusia M;
-    cout << M.get_Kecepatan() << endl;
+    Manusia K;
+    Point P(5,6);
+    K.setPosisi (P);
+    cout << "Posisi ";
+    cout << K.getPosisi().getAbsis() << ", " << K.getPosisi().getOrdinat() << endl;
+    cout << "Prec Posisi ";
+    cout << K.getPrecPosisi().getAbsis() << ", " << K.getPrecPosisi().getOrdinat() << endl;
+    //get_kecepatan
+
+    //Set Gerak Memburu
+    cout << "TES MEMBURU " << endl;
+    M.setMemburu(true);
+    for (int i = 0; i < 9; i++){
+        M.gerak_memburu(P);
+        cout << "Posisi ";
+        cout << M.getPosisi().getAbsis() << ", " << M.getPosisi().getOrdinat() << endl;
+        cout << "Prec Posisi ";
+        cout << M.getPrecPosisi().getAbsis() << ", " << M.getPrecPosisi().getOrdinat() << endl;
+    }
+
+    //Set Gerak Memburu
+    cout << "TES MENGHINDAR " << endl;
+    M.setMenghindar(true);
+    for (int i = 0; i < 9; i++){
+        M.gerak_menjauh(P);
+        cout << "Posisi ";
+        cout << M.getPosisi().getAbsis() << ", " << M.getPosisi().getOrdinat() << endl;
+        cout << "Prec Posisi ";
+        cout << M.getPrecPosisi().getAbsis() << ", " << M.getPrecPosisi().getOrdinat() << endl;
+    }
+
     return 0;
 }
