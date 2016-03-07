9cc65e6be56626a29a8bf9d457a9eacbf2686d99
Aditio Pangestu <aditiopangestu@gmail.com>
2016-03-07 19:44:19 +0700

    implementasi polisi

---
+++ Polisi.cpp
@@ -0,0 +1,10 @@
+void Polisi::Polisi() : Manusia( {
+
+}
+
+Polisi::Polisi(const Polisi&);
+
+Polisi& Polisi::operator= (const Polisi&);
+
+ostream& Polisi::operator<< (ostream&, const Polisi&);
+
--- Polisi.h
+++ Polisi.h
@@ -1,7 +1,18 @@
+#ifndef POLISI_H
+
 #include "Manusia.h"
+#include <iostream>
 
 class Polisi : public Manusia {
 public :
+    void Polisi();
 
+    Polisi(const Polisi&);
+
+    Polisi& operator= (const Polisi&);
+
+    ostream& operator<< (ostream&, const Polisi&);
 
 };
+
+#endif // POLISI_H
