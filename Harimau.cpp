3f082933c5ea6bea89c776b80d1d0518e58c5417
Ali Akbar <aliakbr@outlook.co.id>
2016-03-07 21:41:37 +0700

    Coba tolongin dong, ini aku mau inisialisasi static member predator Harimau jadi 'p' dan 't' tapi ga bisa-bisa, gunanya biar bisa dibuat factory dan ga susah buat harimau nanti

--- Harimau.cpp
+++ Harimau.cpp
@@ -1,6 +1,6 @@
 #include "Harimau.h"
 
-char* Harimau::predatorHewan(new char('p','t'));
+char* Harimau::predatorHarimau(new char('p','t')); //Disini masih bingungs
 
 Harimau::Harimau(int _umur, char _DNA, int _ulangtahun, Point P, int kenyang, int maks, char* tar, int k,
                  int a, bool lambat, int delta) : Karnivora(_umur,_DNA,_ulangtahun,P,kenyang,maks,tar,k,a,lambat,delta)
--- harimau.h
+++ harimau.h
@@ -6,11 +6,11 @@ class Harimau : public Karnivora{
     public :
         Harimau(int _umur = batasumur_Harimau, char _DNA = 'h', int _ulangtahun = ulangtahun_Harimau,Point P = PAwal,
                 int kenyang = maksimum_tingkat_kekenyangan_Harimau, int maks = maksimum_tingkat_kekenyangan_Harimau
-                , char* tar = predatorHewan, int k = kecepatan_Harimau, int a = U, bool lambat = false,
+                , char* tar = predatorHarimau, int k = kecepatan_Harimau, int a = U, bool lambat = false,
                 int delta = deltaKecepatan_Harimau);
         void setHarimauFromPoint(Point P);
     private :
-        static char* predatorHewan;
+        static char* predatorHarimau;
         const static int batasumur_Harimau = 10;
         const static int ulangtahun_Harimau = 2;
         const static int maksimum_tingkat_kekenyangan_Harimau = 10;
