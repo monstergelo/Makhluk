d9eee1c70d84be32a63c3bdf7b66139ba16ebf1a
Ali Akbar <aliakbr@outlook.co.id>
2016-03-07 21:58:20 +0700

    untuk sementara gini dulu bikin Harimaunya, udah bisa harusnya

--- Harimau.cpp
+++ Harimau.cpp
@@ -1,8 +1,15 @@
 #include "Harimau.h"
 
-char* Harimau::predatorHarimau(new char('p','t')); //Disini masih bingungs
+char* Harimau::predatorHarimau = new char[2]; //Masih bingung disini
 
 Harimau::Harimau(int _umur, char _DNA, int _ulangtahun, Point P, int kenyang, int maks, char* tar, int k,
                  int a, bool lambat, int delta) : Karnivora(_umur,_DNA,_ulangtahun,P,kenyang,maks,tar,k,a,lambat,delta)
 {
+    //Untuk sementara dibuat seperti ini dulu
+    setPredator(0,'p');
+    setPredator(1,'t');
+}
+
+void Harimau::setHarimauFromPoint(Point P){
+    setPosisi(P);
 }
