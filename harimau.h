42c4db8a576e1191190a3bc01bc02fc14c67c013
Ali Akbar <aliakbr@outlook.co.id>
2016-03-09 13:56:36 +0700

    Update

--- harimau.h
+++ harimau.h
@@ -2,7 +2,7 @@
 #define HARIMAU_H_INCLUDED
 #include "Karnivora.h"
 
-class Harimau : public Karnivora{
+class Harimau: public Karnivora{
     public :
         Harimau(Point P = PAwal);
         Harimau(const Harimau&);
@@ -12,9 +12,10 @@ class Harimau : public Karnivora{
     private :
         const static int batasumur_Harimau = 10;
         const static int ulangtahun_Harimau = 2;
-        const static int maksimum_tingkat_kekenyangan_Harimau = 10;
+        const static int maksimum_tingkat_kekenyangan_Harimau = 9;
         const static int kecepatan_Harimau = 5;
         const static int deltaKecepatan_Harimau = 2;
 };
+
 
 #endif // HARIMAU_H_INCLUDED
