29e3138e372587e79708ced7fc77dd17a84ef229
Aditio Pangestu <aditiopangestu@gmail.com>
2016-03-10 11:25:29 +0700

    Menusia Doxygen

--- Manusia.h
+++ Manusia.h
@@ -4,50 +4,83 @@
 #include "MakhlukHidup.h"
 #include "HuntingSkill.h"
 #include "Gerak.h"
-
+/**
+ *  A Manusia Class. A Class that describe Human.
+ * This class inherit from MakhlukHidup class, HuntingSkill class and Gerak class.
+ */
 class Manusia : public MakhlukHidup, public HuntingSkill, public Gerak {
 public :
-    //CTOR
+
+    /**
+     * A Constructor.
+     * It use default parameters.
+     * @see MakhlukHidup
+	 * @see HuntingSkill
+	 * @see Gerak
+     */
+
     Manusia(int _umur = 0, char _DNA = '*', int _ulangtahun = 0, Point P = PAwal, char* Target = NULL, bool _memburu = false, int kec = 0, int arah = 0);
 
-    //CCTOR
+    /**
+     * A Copy Constructor.
+     */
     Manusia(const Manusia&);
 
-    //DTOR tidak perlu
-
-    //OPERATOR =
+    /**
+     * An Operator =
+     */
     Manusia& operator= (const Manusia&);
 
+	/**
+	 * Setter procedure for menghindar attribute
+	 * @param m A first parameter will be assigned in menghindar atrribute
+	 */
     void setMenghindar(bool m);
-    /*
-    Mengeset bool menghindar jika ada predator di sekitar.
-    Hal ini digunakan untuk menentukan gerak selanjutnya yaitu
-    menjauh dari predator.
-    */
 
-    void setPosisiPredator(Point Pre);
-    /*
-    Mengeset posisi predator yang diketahui
-    */
-
-    //GETTER
+    /**
+     * Getter function for menhindar attribute
+     * @return menghindar attribute
+     */
     bool getMenghindar ();
-    Point getPosisiPredator();
 
+	/**
+     * A procedure changes posisi attribute and PPosisi atrribute.
+     * It uses random direction.
+     * @see Gerak#gerak_bebas()
+     */
     void gerak_bebas();
-    void gerak_memburu(Point Target);
-    //Membuat gerakan dengan arah menuju suatu titik
+
+    /**
+     * A procedure changes posisi attribute and PPosisi atrribute.
+     * It uses direction that away from Predator.
+     * @param Predator A parameter that passing the predator position.
+     * @see Gerak#gerak_menjauh()
+     */
     void gerak_menjauh(Point Predator);
-    //Membuat gerakan dengan arah menjauhi suatu titik;
+
+    /**
+     * A procedure changes posisi attribute and PPosisi atrribute.
+     * It uses direction that close to Target.
+     * @param Target A parameter that passing the target position.
+     * @see Gerak#gerak_memburu()
+     */
+    void gerak_memburu(Point Target);
+
+    /**
+     * A procedure changes posisi attribute and PPosisi atrribute.
+     * It uses direction that assigned by _arah.
+     * @param _arah A parameter that passing the direction.
+     * @see Gerak#gerak_berarah()
+     */
     void gerak_berarah(int _arah);
-    //Membuat gerakan arah yang ditentukan
 
 private :
+    /**
+     * A private attribute.
+     * It stores the dodge condition.
+     */
     bool menghindar;
-    //Menyimpan kondisi manusia harus menghindar
-    Point PosisiPredator;
-    //Menyimpan posisi dari predator
+
 
 };
-//wawawawawawaw;
 #endif // MANUSIA_H
