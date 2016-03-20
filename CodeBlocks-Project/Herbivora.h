81f3a1880b234f0fd2d40c7c01ef5b5b782ec9ae
harryawk <harry.kambey@gmail.com>
2016-03-20 09:30:30 +0700

    dokumentasi doxygen

--- CodeBlocks-Project/Herbivora.h
+++ CodeBlocks-Project/Herbivora.h
@@ -6,30 +6,65 @@
 //Catatan : Hewan inherit ke MakhlukHidup, Gerak, dan HuntingSkill
 class Herbivora : public Hewan {
 public:
-        Herbivora();
-        Herbivora(int _umur, char _DNA, 
-                  int _ulangtahun, Point P, 
-                  int kenyang, int maks, 
-                  char* tar, bool _memburu, 
-                  int k, int a, 
-                  bool lambat, int delta);
+    /**
+     *
+     */
+    Herbivora();
+    /**
+     *
+     */
+    Herbivora(int _umur, char _DNA, 
+              int _ulangtahun, Point P, 
+              int kenyang, int maks, 
+              char* tar, bool _memburu, 
+              int k, int a, 
+              bool lambat, int delta);
 
-        //CCTOR
-        Herbivora(const Herbivora&);
-        Herbivora& operator=(const Herbivora&);
-        virtual ~Herbivora();
-        void set_percepat( bool cepat);
-        void set_deltaKecepatan(int kec);
-        bool get_percepat();
-        int get_deltaKecepatan();
-        void prosesMempercepat();
-        virtual void Reaction(MakhlukHidup&);
-    //services
+    //CCTOR
+    /**
+     *
+     */
+    Herbivora(const Herbivora&);
+    /**
+     *
+     */
+    Herbivora& operator=(const Herbivora&);
+    /**
+     *
+     */
+    virtual ~Herbivora();
+    /**
+     *
+     */
+    void set_percepat( bool cepat);
+    /**
+     *
+     */
+    void set_deltaKecepatan(int kec);
+    /**
+     *
+     */
+    bool get_percepat();
+    /**
+     *
+     */
+    int get_deltaKecepatan();
+    /**
+     *
+     */
+    void prosesMempercepat();
+    /**
+     *
+     */
+    virtual void Reaction(MakhlukHidup&);
 private:
+    /**
+     *  Store information how fast the Herbivore got to be.
+     */
     int deltaKecepatan;
-    /*
-    Menyimpan nilai peningkatan kecepatan
-    */
+    /**
+     *  Store information is Herbivore need to get faster or not.
+     */
     bool percepat;
 };
 
