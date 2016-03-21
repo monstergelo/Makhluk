d33992f6a1fee1dd1430354560bbeb4f5cf7810a
Ali Akbar <aliakbr@outlook.co.id>
2016-03-21 11:54:37 +0700

    Udah pakek Doxy

--- CodeBlocks-Project/Hewan.h
+++ CodeBlocks-Project/Hewan.h
@@ -1,3 +1,6 @@
+/**
+ *  A Hewan class. A Class that describe a Hewan.
+ */
 #ifndef HEWAN_H
 #define HEWAN_H
 
@@ -7,41 +10,88 @@
 
 class Hewan : public MakhlukHidup, public HuntingSkill, public Gerak{
 public :
-    //5 sekawan
-    Hewan(int _umur = 0, char _DNA = '*', int _ulangtahun = 0, Point P = PAwal, int kenyang = 0, int maks = 0, char* tar = NULL, bool _memburu = false, int k = 0, int a = 0);
-    //~Hewan();
+    /**
+	 * A constructor
+	 * Making an organism with a default value in every parameter
+	 */
+    Hewan(int _umur = 0, char _DNA = '*', int _ulangtahun = 0, 
+		Point P = PAwal, int kenyang = 0, int maks = 0, char* tar = NULL, bool _memburu = false, int k = 0, int a = 0);
+    /**
+	 * A copy constructor
+	 */
     Hewan(const Hewan&);
+    /**
+	 * An operator =
+	 */
     Hewan& operator=(const Hewan&);
+    /**
+	 * A normal procedure member
+	 * A procedure that set the hunger status of the organism
+	 */ 
     void setLapar();
+    /**
+	 * A destructor
+	 * Virtual destructor to destruct an organism
+	 */
     virtual ~Hewan();
-    /*
-    Menghasilkan true jika tingkat kekenyangan rendah.
-    Hal ini mengakitkan skill berburu dari hewan muncul.
-    */
+    /**
+	 * A setter for tingkat_kekenyangan
+	 * @param an integer arguments that will be assigned to tingkat_kekenyangan
+	 */
     void set_tingkat_kekenyangan(int kenyang);
+    /**
+	 * A setter for maks_tingkat_kekenyangan
+	 * @param an integer arguments that will be assigned to maks_tingkat_kekenyangan
+	 */
     void set_maks_tingkat_kekenyangan(int maks);
+    /**
+	 * A getter for tingkat_kekenyangan
+	 * @return an integer
+	 */ 
     int get_tingkat_kekenyangan();
+    /**
+	 * A getter for maks_tingkat_kekenyangan
+	 * @return an integer
+	 */ 
     int get_maks_tingkat_kekenyangan();
+    /**
+	 * A getter for hunger status
+	 * @return a boolean
+	 */ 
     bool get_lapar();
+    /**
+	 * A normal procedure member
+	 * A procedure that processing the death of the animal
+	 */  
     void hewanMati();
+    /**
+	 * A normal procedure member
+	 * A procedure that makes animal move freely
+	 */ 
     void gerak_bebas();
+    /**
+	 * A normal procedure member that take one argument
+	 * @param A Point of the target
+	 * A procedure that makes the animal hunting the target
+	 */ 
     void gerak_memburu(Point Target);
-    //Membuat gerakan dengan arah menuju suatu titik
+    /**
+	 * A normal procedure member
+	 * A procedure that makes animal move in a direction that has been set
+	 */ 
     void gerak_berarah();
-    //Membuat gerakan arah yang ditentukan
 private :
+	/**
+	 * An attribute that save the sated rate of the animal
+	 */
     int tingkat_kekenyangan;
-    /*
-    Menyimpan nilai tingkat kekenyangan dari hewan. Hal ini dapat digunakan
-    untuk menentukan kondisi lapar dan kematian hewan.
-    Contoh : Tingkat kekenyangan = 3, berarti hewan akan mati jika selama 3 detik tidak mendapatkan makanan.
-    */
-
+    /**
+	 * An attribute that save the maximum sated rate of the animal 
+	 */
     int maks_tingkat_kekenyangan;
-    /*
-    Menyimpan nilai maksimum dari tingkat kekenyangan hewan.
-    Hal ini digunakan untuk memberi batasan dari tingkat kekenyangan hewan tersebut.
-    */
+    /**
+	 * An attribute that save the hunger state of the animal
+	 */
     bool Lapar;
 };
 
