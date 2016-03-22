bf51ce8291ccfa1af5c64191f9b791808f439577
harryawk <harry.kambey@gmail.com>
2016-03-22 15:52:55 +0700

    Doxy get

--- S01-B/Tumbuhan.h
+++ S01-B/Tumbuhan.h
@@ -2,17 +2,37 @@
 #define TUMBUHAN_H
 
 #include "MakhlukHidup.h"
-
+/**
+ *  A Tumbuhan class. A Class that describe a Plant.
+ */
 class Tumbuhan : public MakhlukHidup {
-public:
-	Tumbuhan(int=0 , char='*' , int=0 , Point=PAwal ,char* predators=NULL);
-	virtual ~Tumbuhan();
-	Tumbuhan(const Tumbuhan&);
-	Tumbuhan& operator=(const Tumbuhan&);
- 
- 	virtual void Reaction(MakhlukHidup&);
-private:
-
+	public:
+		/*
+		 * A constructor with 5 parameter
+		 * @param an integer for the age limit of the animal
+		 * @param a character of the carnivore's DNA
+		 * @param an integer "birthday" that saves the birthday time for animal
+		 * @param a Point that tells the position of the Carnivore
+		 * @param a List of char that contain the predator of the plant
+		 * */
+		Tumbuhan(int=0 , char='*' , int=0 , Point=PAwal ,char* predators=NULL);
+		/**
+		 * A virtual destructor
+		 */
+		virtual ~Tumbuhan();
+		/**
+		 * A copy constructor
+		 */
+		Tumbuhan(const Tumbuhan&);
+		/**
+		 * An operator=
+		 */
+		Tumbuhan& operator=(const Tumbuhan&);
+		/**
+		 * A virtual member procedure
+		 * Process Reaction make plant react to the other organism
+		 */
+		virtual void Reaction(MakhlukHidup&);
 };
 
 #endif
--- S01-B/konduktorMakhlukHidup.h
+++ S01-B/konduktorMakhlukHidup.h
@@ -9,37 +9,115 @@
 #include "Tumbuhan.h"
 #include <thread>
 
-
+/**
+ *  A class to control a MakhlukHidup
+ */
 //class untuk mengatur kehidupan makhluk hidup
 class KonduktorMakhlukHidup
 {
 public:
 	//ctor, dtor
+	/**
+	 *  constructor which initialize lifestate with 1
+	 */
 	KonduktorMakhlukHidup();
+	/**
+	 *  a destructor
+	 */
 	~KonduktorMakhlukHidup();
-
-
 
 	//method
 	/**
- 	* 	Told Manusia to do it's behaviour
- 	*/
-
-	void hidup(MakhlukHidup&); //menghidupkan semua makhluk hidup
-	void hidup(Manusia&); //menghidupkan semua makhluk hidup
-	void hidup(Herbivora&);
-	void hidup(Karnivora&);
-	void hidup(Omnivora&);
-	void hidup(Tumbuhan&);
+ 	* 	Told a Manusia to do it's behaviour
+ 	* 	@param a Manusia
+ 	*/	
+	void hidup(Manusia&);
+	/**
+	 *  Set the value of Count
+	 * 	@param an integer
+	 */
 	void setCount(int c);
+	/**
+	 *  returns the value of Count
+	 *	@return an integer
+	 */
 	int getCount();
+	/**
+	 *  Told a Herbivora to do it's behaviour
+	 * 	@param a MakhlukHidup
+	 */
+	void hidup(MakhlukHidup&);
+	/**
+	 *  Told a Herbivora to do it's behaviour
+	 * 	@param a Herbivore
+	 */
+	void hidup(Herbivora&);
+	/**
+	 *  Told a Karnivora to do it's behaviour
+	 *  @param a Karnivora
+	 */
+	void hidup(Karnivora&);
+	/**
+	 *  Told a Omnivora to do it's behaviour
+	 * 	@param a Omnivora
+	 */
+	void hidup(Omnivora&);
+	/**
+	 *  Told a Tumbuhan to do it's behaviour
+	 * 	@param a Tumbuhan
+	 */
+	void hidup(Tumbuhan&);
+	/**
+	 *  prevent all behavior to be done
+	 */
 	void pause();
+	/**
+	 *  cancel a pause
+	 */
 	void resume();
+	/**
+	 *  Increase the umur of a MakhlukHidup
+	 * 	@param a MakhlukHidup
+	 */
 	void aging(MakhlukHidup&);
+	/**
+	 *  Set the value of batasBawah
+	 *  @param an integer
+	 */
+	void setBawah(int);
+	/**
+	 *  Set the value of batasSamping
+	 * 	@param an integer
+	 */
+	void setSamping(int);
+	/**
+	 *  returns the value of batasBawah
+	 * 	@return an integer
+	 */
+	int GetBawah();
+	/**
+	 *  returns the value of batasSamping
+	 * 	@return an integer
+	 */
+	int GetSamping();
 
 private:
+	/**
+	 *  a flag to pause method hidup
+	 */
 	int lifeState;
-	int Count;
+	/**
+	 *  bottom edge of map
+	 */
+	int batasBawah;
+	/**
+	 *  right edge of map
+	 */
+	int batasSamping;
+	/**
+	 *  total of MakhlukHidup inside the list
+	 */
+	 int Count;
 };
 
 #endif
